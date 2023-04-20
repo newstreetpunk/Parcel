79d965c6607fbdc944147909005ea10a611fd07e
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-04-20 20:01:56 +0500

    build

---
+++ docs/js/app.25bfb236.js
@@ -0,0 +1,8 @@
+function $7cbbeaa003d6deff$export$e16d8520af44a096(a, b) {
+    console.log(a + b);
+}
+
+
+(0, $7cbbeaa003d6deff$export$e16d8520af44a096)(4, 5);
+
+
---
+++ docs/js/app.80b74a1d.js
@@ -0,0 +1,9 @@
+(function () {
+function $6897f58233550955$export$e16d8520af44a096(a, b) {
+    console.log(a + b);
+}
+
+
+(0, $6897f58233550955$export$e16d8520af44a096)(4, 5);
+
+})();
